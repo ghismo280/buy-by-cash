761dd40fc23384ec1af084dd2f2db0d8760d9f5b
luis chagas <luischagas78@gmail.com>
2018-10-31 12:44:18 -0400

    seeeeeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+puts "Deleting all Users"
 
+User.destroy_all
+
+# USER --------------------------------------
+puts "Creating eBuyers"
 10.times do
-  ebuyer = Ebuyer.new(
-    name:    Faker::Artist.name,
-    surname: Faker::Company.name,
+  ebuyer = User.new(
+    name: Faker::Name.first_name,
+    surname: Faker::Name.middle_name,
     email: Faker::Internet.email,
-    address: Faker::Address.city},
-    rating:  rand(0..5)
+    city: Faker::Address.city,
+    ebuyer: true,
+    password: "123456"
   )
   ebuyer.save!
+end
+
+puts "Creating Customers"
+10.times do
+  customer = User.new(
+    name: Faker::Name.first_name,
+    surname: Faker::Name.middle_name,
+    email: Faker::Internet.email,
+    city: Faker::Address.city,
+    ebuyer: false,
+    password: "123456"
+  )
+  customer.save!
+end
+
+
+# CONVERSATION -----------------------------------
+puts "Creating Conversations"
+10.times do
+  conversation = Conversation.new(
+    string: Faker::Lebowski.quote,
+  )
+  conversation.save!
+end
+
+
+# MESSAGE -----------------------------------
+puts "Creating Messages"
+10.times do
+  message = Message.new(
+    body: Faker::Seinfeld.quote,
+  )
+  message.save!
+end
+
+
+# REVIEW -----------------------------------
+puts "Creating Reviews"
+10.times do
+  review = Review.new(
+    body: Faker::SiliconValley.quote,
+    stars:  rand(0..5)
+  )
+  review.save!
 end
