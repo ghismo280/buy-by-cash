4dfdcf356643c14058dfedaf2450e0cb8abfecc8
Angelo Callari <angelo280@hotmail.it>
2018-10-31 19:57:15 +0100

    map completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,3 @@
 import "bootstrap";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWhhaGFlM2V3MjNlIiwiYSI6ImNqbmVsZHBhZzB2aGkzcHNmODEzNHRzeHAifQ.pkiyIJ4458uLJ2ILrUMkGA';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-74.50, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
+
---
+++ app/javascript/packs/map.js
@@ -0,0 +1,9 @@
+
+
+  mapboxgl.accessToken = window._rails_env
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
