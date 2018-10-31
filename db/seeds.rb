73fa1cec10ffb492fa4e5110d2a3fd8e97a0e5d1
luis chagas <luischagas78@gmail.com>
2018-10-31 11:33:05 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  ebuyer = Ebuyer.new(
+    name:    Faker::Artist.name,
+    surname: Faker::Company.name,
+    email: Faker::Internet.email,
+    address: Faker::Address.city},
+    rating:  rand(0..5)
+  )
+  ebuyer.save!
+end
