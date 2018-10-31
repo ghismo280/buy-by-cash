06d1b567393d2f87f70728a9f8dde511b5886e1f
Angelo Callari <angelo280@hotmail.it>
2018-10-31 17:12:44 +0100

    polymorphic conversation messages from NASA

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..30).each do |num|
+  a = User.new(email: "#{num}mail@gmail.com", password:'1q2w3e44iuhqurhq',
+                name:"#{num} name", surname:"#{num} surname", city: "#{num}city")
+  if num % rand(2) == 0
+    bool = true
+  else
+    bool = false
+  end
+  a.ebuyer = bool
+  a.save!
+end
