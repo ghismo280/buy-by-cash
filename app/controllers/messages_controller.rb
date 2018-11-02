e3d7c6a33a11677f4fc4fa51d0caac5e8cf02e84
Angelo Callari <angelo280@hotmail.it>
2018-11-02 17:59:12 +0100

    added link to accept the job

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,14 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.where(conversation: Conversation.where(recipient: current_user))
   end
+
+  def accept
+    @a = Conversation.where(recipient: current_user, sender: User.find(params[:sender_id].to_i)).first
+    if @a.ask_for_job == true
+      @a.accept_job = true
+      redirect_to '/messages'
+    else
+      flash[:success] = "Woohoo!"
+    end
+  end
 end
